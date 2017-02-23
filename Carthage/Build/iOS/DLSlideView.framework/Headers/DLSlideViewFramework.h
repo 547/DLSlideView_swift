7811334fe42484b6fb50d37729cd09857204bf64
547 <sevenwang@SevenWangdeiMac.local>
2017-02-22 17:01:59 -0700

    打包成Framework
    使用Carthage

---
+++ Carthage/Build/iOS/DLSlideView.framework/Headers/DLSlideViewFramework.h
@@ -0,0 +1,19 @@
+//
+//  DLSlideViewFramework.h
+//  DLSlideViewFramework
+//
+//  Created by SevenWang on 2017/2/22.
+//  Copyright © 2017年 SevenWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DLSlideViewFramework.
+FOUNDATION_EXPORT double DLSlideViewFrameworkVersionNumber;
+
+//! Project version string for DLSlideViewFramework.
+FOUNDATION_EXPORT const unsigned char DLSlideViewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DLSlideViewFramework/PublicHeader.h>
+
+
---
+++ DLSlideView_swift/DLSlideView/DLSlideView_swift-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "DLSlideView.h"
+#import "DLCustomSlideView.h"
+#import "DLTabedSlideView.h"
+#import "DLUtility.h"
+#import "DLFixedTabbarView.h"
+#import "DLScrollTabbarView.h"
+#import "DLSlideTabbarProtocol.h"
+#import "DLCacheProtocol.h"
+#import "DLLRUCache.h"
