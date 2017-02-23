e6c07067f38833e09bd8c4ecf56ea233d0fd6ba6
547 <sevenwang@SevenWangdeiMac.local>
2017-02-23 09:06:03 -0700

    再次打包成framework

---
+++ DLSlideView_swift/DLSlideView/DLSlideViewHeader.h
@@ -0,0 +1,22 @@
+//
+//  DLSlideViewHeader.h
+//  DLSlideView_swift
+//
+//  Created by SevenWang on 2017/2/23.
+//  Copyright © 2017年 SevenWang. All rights reserved.
+//
+
+#ifndef DLSlideViewHeader_h
+#define DLSlideViewHeader_h
+#import "DLSlideView.h"
+#import "DLCustomSlideView.h"
+#import "DLTabedSlideView.h"
+#import "DLUtility.h"
+#import "DLFixedTabbarView.h"
+#import "DLScrollTabbarView.h"
+#import "DLSlideTabbarProtocol.h"
+#import "DLCacheProtocol.h"
+#import "DLLRUCache.h"
+
+
+#endif /* DLSlideViewHeader_h */
--- DLSlideView_swift/DLSlideView/DLSlideView_swift-Bridging-Header.h
+++ DLSlideView_swift/DLSlideView/DLSlideView_swift-Bridging-Header.h
@@ -2,12 +2,4 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "DLSlideView.h"
-#import "DLCustomSlideView.h"
-#import "DLTabedSlideView.h"
-#import "DLUtility.h"
-#import "DLFixedTabbarView.h"
-#import "DLScrollTabbarView.h"
-#import "DLSlideTabbarProtocol.h"
-#import "DLCacheProtocol.h"
-#import "DLLRUCache.h"
+#import "DLSlideViewHeader.h"
